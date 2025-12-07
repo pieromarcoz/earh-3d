95a1a0a40f59fa425d0ab895805e2a89f3273a92
pieromarcoz <75951868+pieromarcoz@users.noreply.github.com>
2025-12-07 05:31:20 -0500

    fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import './globals.css'
 import { JetBrains_Mono } from 'next/font/google'
-
+import { Analytics } from "@vercel/analytics/next"
 const jetbrainsMono = JetBrains_Mono({
   subsets: ['latin'],
   weight: ['400', '700'],
@@ -17,6 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${jetbrainsMono.variable} ${jetbrainsMono.className}`}>
       <body className="bg-black text-white font-mono">{children}</body>
+      <Analytics />
     </html>
   )
 }
